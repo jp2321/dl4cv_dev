fef79a3cb01562f7a07fadf66f1f1ca6fe4ab542
Jan-Peter Kucklick <596-kucklick@users.noreply.git.uni-paderborn.de>
2020-03-20 13:39:38 +0100

    Updates

--- exercises/solution_02_04.py
+++ exercises/solution_02_04.py
@@ -2,7 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 from skimage.feature import hog
 
-image = cv2.imread("exercise/dog.jpeg", 0) # Read image as greyscale
+image = cv2.imread("exercises/dog.jpeg", 0) # Read image as greyscale
 resize_img = cv2.resize(image, (300,300)) # Resize image
 
 fd, hog_image = hog(resize_img, orientations=5, pixels_per_cell=(8, 8),
--- exercises/test_02_04.py
+++ exercises/test_02_04.py
@@ -1,4 +1,5 @@
 def test():
+    import numpy as np
     # Here we can either check objects created in the solution code, or the
     # string value of the solution, available as __solution__. A helper for
     # printing formatted messages is available as __msg__. See the testTemplate
@@ -9,7 +10,7 @@ def test():
     fd_2, hog_image_2 = hog(resize_img, orientations=5, pixels_per_cell=(8, 8),
                     cells_per_block=(2, 2), visualize=True, multichannel=False)
 
-    assert  np.array_equal(hog_image, hog_image_2)==True, "Check the pixels per cell"
-    assert  np.array_equal(resize_img, resize_img_2)==True, "Did you resize the image to the right size"
+    assert  np.array_equal(fd, fd_2)==True, "Check the parameters of the hog function"
+    assert  np.array_equal(resize_img, resize_img_2)==True, "Did you resize the image to the right size?"
 
     __msg__.good("Perfect!")
